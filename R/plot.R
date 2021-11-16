cadcf1437a499ca55017cd1e1c4f80005e7813bb
Ali Taqi <alif.taqi00@gmail.com>
2021-11-16 02:19:54 -0500

    Almost done

--- R/plot.R
+++ R/plot.R
@@ -1,1 +1,8 @@
 
+
+.plot_temp_cnt <- function(data){
+  data %>% 
+    ggplot() +
+    geom_point(aes(x = instant, color = atemp, y = cnt)) +
+    labs(title = "Bike User Count in 2011 by Temperature")
+}

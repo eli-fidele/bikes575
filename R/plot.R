d1f8a973cbe21f0c6e3b73a683f2a07200f80b9c
Ali Taqi <alif.taqi00@gmail.com>
2021-11-16 03:34:14 -0500

    Report done

--- R/plot.R
+++ R/plot.R
@@ -4,5 +4,6 @@
   data %>% 
     ggplot() +
     geom_point(aes(x = instant, color = atemp, y = cnt)) +
-    labs(title = "Bike User Count in 2011 by Temperature")
+    labs(title = "Bike User Count in 2011 by Temperature over Time") +
+    scale_color_gradient(low="deepskyblue1", high = "tomato2")
 }
